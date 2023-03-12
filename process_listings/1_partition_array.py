b2865e9705ee2c896da854fc2b0ce9af0b8bdf63
Shea <89158603+sheacon@users.noreply.github.com>
2023-03-12 17:00:39 -0500

    update

--- process_listings/1_partition_array.py
+++ process_listings/1_partition_array.py
@@ -2,21 +2,24 @@ import pandas as pd
 import pyarrow
 import os
 import sys
+from glob import glob
 
 input_dir = sys.argv[1]
-file = sys.argv[2]
+file_section = int(sys.argv[2])
 output_dir = sys.argv[3]
 
 # debug
 #input_dir = "/data/p_dsi/capstone_projects/shea/0_processed/full_data/"
-#file = "part-00000-ab3867d4-3d1a-404d-aadb-d7f3afb6fd40-c000.snappy.parquet"
+#file_section = 2
 #output_dir = "/data/p_dsi/capstone_projects/shea/1_partitioned_array/"
 
+file_list = glob(input_dir + '*.parquet')
+files_per_section = len(file_list)//99
+file_sublist = file_list[(file_section-1)*files_per_section:file_section*files_per_section]
+
 # read in the parquet file
-df = pd.read_parquet(input_dir+file)
-
+df = pd.concat(pd.read_parquet(f) for f in file_sublist)
 df = df.dropna(subset = ["scraped_at"])
-
 df["scraped_at_year"] = pd.to_datetime(df['scraped_at'], unit='s').dt.year
 
 # partition the data
@@ -26,4 +29,4 @@ partitions = df.groupby(["state", "scraped_at_year"])
 for (state, scraped_at_year), partition in partitions:
     partition_path = os.path.join(output_dir, state, str(scraped_at_year))
     os.makedirs(partition_path, exist_ok=True)
-    partition.to_parquet(partition_path + "/" + file)
+    partition.to_parquet(f"{partition_path}/file_{file_section}.parquet")

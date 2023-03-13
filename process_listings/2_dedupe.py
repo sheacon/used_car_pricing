e3afd27388c2672c0e2ce8a3db3b7847903fe295
Shea <89158603+sheacon@users.noreply.github.com>
2023-03-13 09:26:25 -0500

    skylake constraint

--- process_listings/2_dedupe.py
+++ process_listings/2_dedupe.py
@@ -8,8 +8,8 @@ state = sys.argv[1]
 year = int(sys.argv[2])
 
 # debug
-#state = "TN"
-#year = 0
+#state = "TX"
+#year = 4
 
 # determine year
 years = ['2018', '2019', '2020', '2021', '2022']
@@ -22,12 +22,6 @@ input_dir = f"{base_dir}/{state}/{year}/"
 # read files
 df = pd.read_parquet(input_dir)
 
-# NEXT OPTION: UNNEST hvf_options
-# df["hvf_standard"] = df["hvf_options"][0]
-# df["hvf_optional"] = df["hvf_options"][1]
-
-# MAYBE DEPENDS ON NODE SOFTWARE?
-
 # dedupe vin by status_date
 mask = df['status_date'] == df.groupby('vin')['status_date'].transform(max)
 deduped_df = df.loc[mask]
@@ -35,3 +29,6 @@ deduped_df = df.loc[mask]
 # write out
 output_dir = "/data/p_dsi/capstone_projects/shea/2_deduped"
 deduped_df.to_parquet(f"{output_dir}/{state}_{year}.parquet")
+
+# debug read
+#df = pd.read_parquet("/data/p_dsi/capstone_projects/shea/2_deduped/TX_2022.parquet")

d6aa950f49c7069a2ab289f8ac340e09583e3af2
Shea <89158603+sheacon@users.noreply.github.com>
2023-03-14 02:41:04 -0500

    update

--- process_listings/2_dedupe.py
+++ process_listings/2_dedupe.py
@@ -1,15 +1,16 @@
 
+#export ARROW_MEMORY_POOL_MAX_MEMORY=4GB
+
 import sys
-import pandas as pd
-import pyarrow
-from glob import glob
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 state = sys.argv[1]
 year = int(sys.argv[2])
 
 # debug
 #state = "TN"
-#year = 0
+#year = 2
 
 # determine year
 years = ['2018', '2019', '2020', '2021', '2022']
@@ -17,20 +18,23 @@ year = years[year]
 
 # file structure
 base_dir = "/data/p_dsi/capstone_projects/shea/1_partitioned"
-input_dir = f"{base_dir}/{state}/{year}/"
-file_list = glob(input_dir + '*.parquet')
+input_dir = f"{base_dir}/state={state}/status_date_year={year}/"
+#input_dir = "/data/p_dsi/capstone_projects/shea/1_partitioned/state=TN/status_date_year=2020"
 
 # read files
-df = pd.concat(pd.read_parquet(f) for f in file_list)
+table = pq.read_table(input_dir)
+schema = table.schema
 
 # dedupe vin by status_date
-mask = df['status_date'] == df.groupby('vin')['status_date'].transform(max)
-deduped_df = df.loc[mask]
+sorted_table = table.sort_by([('vin', 'ascending'), ('status_date', 'descending')])
+group_cols = [f.name for f in schema if f.name not in ('vin', 'status_date')]
+grouped_table = sorted_table.groupby('vin').aggregate([
+    ('status_date', 'first'),
+    *[(c, 'first') for c in group_cols],
+])
 
 # write out
-output_dir = "/data/p_dsi/capstone_projects/shea/2_deduped"
-deduped_df.to_parquet(f"{output_dir}/{state}_{year}.parquet")
-
-# debug read
-# pd.read_parquet("/data/p_dsi/capstone_projects/shea/1_partitioned/TN/2018/file_4.parquet")
-#df = pd.read_parquet("/data/p_dsi/capstone_projects/shea/2_deduped/TN_2018.parquet")
+output_dir = "/data/p_dsi/capstone_projects/shea/2_deduped/"
+output_file = f"{state}_{year}.parquet"
+pq.write_table(table, output_dir+output_file)
+)

6f6e20cd7e7694b467d9996e30af0392c6ec3086
Shea <89158603+sheacon@users.noreply.github.com>
2023-03-15 14:09:27 -0500

    Update 3_final_listings.py

--- process_listings/3_final_listings.py
+++ process_listings/3_final_listings.py
@@ -7,21 +7,6 @@ state = int(sys.argv[1])
 states = ["TN","TX","OH"]
 state = states[state]
 
-
-all_cols = ['vin', 'price', 'miles', 'year', 'make', 'model', 'trim',
-       'vehicle_type', 'body_type', 'body_subtype', 'drivetrain', 'fuel_type',
-       'engine_block', 'engine_size', 'transmission', 'doors', 'cylinders',
-       'city_mpg', 'highway_mpg', 'base_exterior_color', 'base_interior_color',
-       'is_certified', 'is_transfer', 'scraped_at', 'status_date',
-       'first_scraped_at', 'city', 'state', 'zip', 'latitude', 'longitude',
-       'dealer_type', 'seller_comments', 'currency_indicator',
-       'miles_indicator', 'photo_links_count', 'listed_options',
-       'hvf_options']
-
-read_cols = all_cols
-
-import os
-
 pickle_directory = "/data/p_dsi/capstone_projects/shea/2_deduped/" + state
 pickle_files = [file for file in os.listdir(pickle_directory) if file.endswith('.pickle')]
 
